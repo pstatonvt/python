42c7a15a156e1a3c2107af9835acff28a630f383
pstatonvt <30532152+pstatonvt@users.noreply.github.com>
2018-03-05 14:08:48 -0600

    Add files via upload

---
+++ Python_For_Everybody/Course_2/Assignment_9.4.py
@@ -0,0 +1,34 @@
+'''
+9.4 Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail.
+
+The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file.
+
+After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+'''
+fh = open("mbox-short.txt","r")
+storage = dict()
+lst = list()
+
+for line in fh:
+    line = line.rstrip()
+    if line.startswith("From "):
+        line = line.split()
+        tmp = line[1]
+        lst.append(tmp)
+
+for name in lst:
+    storage[name] = storage.get(name, 0) + 1
+print("Dictionary: ",storage)
+print()
+
+largest = None
+for x in storage.values():
+    if largest is None:
+        largest = x
+    if x > largest:
+        largest = x
+print("Largest: ",largest)
+print()
+
+for key in storage:
+        
