8f038920ffdbbc92817f7c88e85da76a9922728b
pstatonvt <pstaton@vt.edu>
2018-03-14 13:48:30 -0500

    added the 'scraping_numbers' assignment

---
+++ Python_For_Everybody/Course_3/scraping_numbers.py
@@ -0,0 +1,15 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+#find span tags
