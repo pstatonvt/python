2a122f4a029f413f47eea3f15846cebb431bf1b4
pstatonvt <30532152+pstatonvt@users.noreply.github.com>
2018-03-15 08:54:00 -0500

    completed assignment

--- Python_For_Everybody/Course_3/scraping_numbers.py
+++ Python_For_Everybody/Course_3/scraping_numbers.py
@@ -2,22 +2,26 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
+array = []
+counter = 0
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-#url = input('Enter - ')
-#sample data: http://py4e-data.dr-chuck.net/comments_42.html
-url = 'http://py4e-data.dr-chuck.net/comments_42.html'
+url = 'http://py4e-data.dr-chuck.net/comments_72206.html'
 html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
-print(soup)
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
+
+# Retrieve all of the anchor numberss
+span = soup('span')
+
+#isolating the numbers in the column, appending them to a list, and summing them
+for x in span:
+    counter += 1
+    x.contents[0] = int(x.contents[0])
+    array.append(x.contents[0])
+
+print("Count", counter)
+print("Sum", sum(array))
