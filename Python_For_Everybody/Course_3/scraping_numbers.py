d3524008298638b8f09672225b354c398d28e359
pstatonvt <pstaton@vt.edu>
2018-03-23 12:13:55 -0500

    edits

--- Python_For_Everybody/Course_3/scraping_numbers.py
+++ Python_For_Everybody/Course_3/scraping_numbers.py
@@ -1,4 +1,4 @@
-import urllib.request, urllib.parse, urllib.error
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
 
@@ -11,5 +11,12 @@ url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
 
 soup = BeautifulSoup(html, "html.parser")
-
-#find span tags
+print(soup)
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
